b63e1d59e5279fa9b2d427e9d6ca794531521edb
kaishui <yiqiesuifengf@sina.com>
2020-02-02 16:49:53 +0800

    FSC

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+const App = () => {
+    return <div>Hello world for zijing</div>
+}
+
 ReactDOM.render(
-    <div>Hello world</div>,
+    <App/>,
     document.getElementById('root')
 );
